978d6080ed4bd34d8a707328541ce24b710bfe64
sder12 <sara.cetto@gmail.com>
2022-10-12 15:29:02 +0200

    Milestone 2: create dinamic element with Js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,35 @@
-// DEBUG 
-console.log("Hello");
+// MILESTONE 2
+//INSERIRE IMG IN MODO DINAMICO JS
+//creare array con immagini
+//creare elemento con template literal (.innerHTML)
+//dare la classe active solo alla prima img
+
+const sliderImg = [
+  "img/01.jpg",
+  "img/02.jpg",
+  "img/03.jpg",
+  "img/04.jpg",
+  "img/05.jpg",
+];
+
+// Prelevare da HTML
+const slider = document.querySelector(".slider");
+console.log(sliderImg);
+console.log(slider);
+//Btn const
+const prevUp = document.querySelector(".prev");
+const nextDown = document.querySelector(".next");
+console.log(prevUp, nextDown);
+
+//creare div img
+for (let i = 0; i < sliderImg.length; i++) {
+  const thisImg = sliderImg[i];
+  console.log(thisImg);
+  slider.innerHTML += `<div class="image">
+                            <img src="${thisImg}" alt="">
+                    </div>`;
+}
+
+const imgDiv = document.querySelector(".image");
+console.log(imgDiv);
+imgDiv.classList.add("active")
