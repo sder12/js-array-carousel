48200d6227cd492bf17dbd24660a2539dc71d010
sder12 <sara.cetto@gmail.com>
2022-10-12 17:02:21 +0200

    Bonus 1: infinite carousel

--- js/script.js
+++ js/script.js
@@ -29,40 +29,58 @@ for (let i = 0; i < sliderImg.length; i++) {
 //dare active
 const imgDiv = document.getElementsByClassName("image");
 console.log(imgDiv);
-//NB uso elements by class name perchè prendo tutti i div 
+//NB uso elements by class name perchè prendo tutti i div
 let sliderPosition = 0;
-imgDiv[sliderPosition].classList.add("active")
+imgDiv[sliderPosition].classList.add("active");
 console.log(sliderPosition);
 
 // MILESTONE 3
 // Al click sul bottone eventListener
-// la classe active si cancella dall'img precedente 
+// la classe active si cancella dall'img precedente
 // la classe active si aggiunge alla nuova img IN SUCESSIONE++
+// BONUS 1: ciclo infinito del carosello.
 
 //Btn const
 const prevUp = document.querySelector(".prev");
 const nextDown = document.querySelector(".next");
 console.log(prevUp, nextDown);
 
-nextDown.addEventListener("click", function(){
+nextDown.addEventListener("click", function () {
+  if (sliderPosition < imgDiv.length - 1) {
     //rimuovo active
-    imgDiv[sliderPosition].classList.remove("active")
-    // assegno active a img dopo 
-    sliderPosition++
-    imgDiv[sliderPosition].classList.add("active")
+    imgDiv[sliderPosition].classList.remove("active");
+    // assegno active a img dopo
+    sliderPosition++;
+    imgDiv[sliderPosition].classList.add("active");
     //bloccare l'aumento di slider position
-})
-console.log(sliderPosition)
+  } else {
+    imgDiv[sliderPosition].classList.remove("active");
+    sliderPosition = 0;
+    imgDiv[sliderPosition].classList.add("active");
+  }
+});
 
-
-prevUp.addEventListener("click", function(){
+prevUp.addEventListener("click", function () {
+  if (sliderPosition > 0 && sliderPosition <= imgDiv.length - 1) {
     //rimuovo active
-    imgDiv[sliderPosition].classList.remove("active")
-    // assegno active a img dopo 
-    sliderPosition++
-    imgDiv[sliderPosition].classList.add("active")
+    imgDiv[sliderPosition].classList.remove("active");
+    // assegno active a img dopo
+    sliderPosition--;
+    imgDiv[sliderPosition].classList.add("active");
     //bloccare l'aumento di slider position
-})
-console.log(sliderPosition)
+  } else {
+    imgDiv[sliderPosition].classList.remove("active");
+    sliderPosition = imgDiv.length - 1;
+    imgDiv[sliderPosition].classList.add("active");
+  }
+});
 
-//if(sliderPosition > sliderImg.length){...}
+//-----------------------------------------------------------
+// BONUS 2:
+// Aggiungere la visualizzazione di tutte le thumbnails
+// sulla destra dell’immagine grande attiva, come nello screenshot
+// proposto. Tutte le miniature avranno un layer di opacità scura,
+// tranne quella corrispondente all’immagine attiva, che invece
+// avrà un bordo colorato.
+// Al click delle frecce, oltre al cambio di immagine attiva,
+// gestire il cambio di miniatura attiva.
